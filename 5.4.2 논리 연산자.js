bbb0419c0a316b728db8392fee302143ba1f4a73
haseungyeon <gktmdudl@korea.ac.kr>
2022-09-07 21:57:21 +0900

    6.3.3 이벤트 처리까지

---
+++ 5.4.2 논리 연산자.js
@@ -0,0 +1,4 @@
+p = {name:"Tom", age:18};
+console.log(p && p.name);
+console.log(p && p.hobby);
+console.log(p && p.name && p.age)
---
+++ 6.2.3 객체의 프로퍼티.js
@@ -0,0 +1,4 @@
+console.time("operate")
+var p = {x:1, y:2}
+console.dir(p)
+console.timeEnd("operate")
